317a8782fdd20b4025a9b0cfc0dbd6721bfefed4
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-08-21 14:20:54 -0700

    Time: 79 ms (44.20%), Space: 44.1 MB (60.99%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,15 +1,7 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        int n = s.length();
-        for(int i = 1; i <= n/2; i++) {
-            if (n%i==0) {
-                StringBuilder sb = new StringBuilder();
-                for (int j = 0; j < n/i; j++) {
-                    sb.append(s.substring(0,i));
-                }
-                if (s.equals(sb.toString())) return true;
-            }
-        }
+        String t = s + s;
+        if((t.substring(1, t.length()-1)).contains(s)) return true;
         return false;
     }
 }
