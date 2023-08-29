3933d6ca26666be6a5efd7def153335a3f666699
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-08-29 13:24:24 -0700

    Time: 241 ms (32.62%), Space: 50.5 MB (75.61%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,16 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        HashSet<List<Integer>> res = new HashSet<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (map.containsKey(0-nums[i]-nums[j])) {
-                    res.add(new LinkedList<>(Arrays.asList(nums[i], nums[j], 0-nums[i]-nums[j])));
+        HashSet<List<Integer>> combos = new HashSet<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int l = i + 1;
+            int r = n - 1;
+            while (l < r) {
+                if (nums[i] + nums[l] + nums[r] == 0) {
+                    combos.add(new ArrayList<>(Arrays.asList(nums[i], nums[l++], nums[r--])));
+                } else if (nums[i] + nums[l] + nums[r] > 0) {
+                    r--;
+                } else {
+                    l++;
                 }
             }
-            map.put(nums[i], i);
         }
-        return new LinkedList(res);
+        List<List<Integer>> res = new ArrayList<>();
+        res.addAll(combos);
+        return res;
     }
 }
