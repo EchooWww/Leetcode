b525e458eeb17bd109b666ca7e5b554bc13cc2a7
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-09-04 01:46:58 -0700

    Time: 3 ms (31.31%), Space: 47 MB (7.51%) - LeetHub

---
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.java
@@ -0,0 +1,21 @@
+class Solution {
+    List<List<Integer>> res = new LinkedList<>();
+    public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+        List<Integer> path = new LinkedList<>();
+        traverse(graph, 0, path);
+        return res;
+    }
+    
+    public void traverse(int[][] graph, int s, List<Integer> path) {
+        path.add(s);
+        if (s == graph.length -1) {
+            res.add(new LinkedList<>(path));
+        }
+        
+        for (int nb: graph[s]) {
+            traverse(graph, nb, path);
+        }
+        
+        path.remove(path.size()-1);
+    } 
+}
