92c2980265b68ba8df026d5a540e6f153139a285
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-09-12 10:26:07 -0700

    Time: 49 ms (29.15%), Space: 44.5 MB (65.1%) - LeetHub

--- 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,20 +1,40 @@
 class Solution {
     public int minDeletions(String s) {
-        Integer[] freq = new Integer[26];
-        for (int i = 0; i < 26; i++) {
-            freq[i] = 0; // Initialize each element to 0
-        }
+        HashMap<Character, Integer> freq = new HashMap<>();
         for (char c: s.toCharArray()) {
-            freq[c-'a']++;
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
         }
-        Arrays.sort(freq, (a, b) ->b-a);
-        int res = 0;
-        for (int i = 1; i < 26; i++) {
-            while (freq[i] != 0 && freq[i] >= freq[i-1]) {
-                freq[i]--;
-                res++;
+        HashSet<Integer> usedFreq = new HashSet<>();
+        int steps = 0;
+        for (int n :freq.values()) {
+            while (n > 0 && usedFreq.contains(n)) {
+                n--;
+                steps++;
             }
+            usedFreq.add(n);
         }
-        return res;
+        return steps;
     }
 }
+
+// class Solution {
+//     public int minDeletions(String s) {
+//         Integer[] freq = new Integer[26];
+//         for (int i = 0; i < 26; i++) {
+//             freq[i] = 0; 
+//         }
+//         for (char c: s.toCharArray()) {
+//             freq[c-'a']++;
+//         }
+//         Arrays.sort(freq, (a, b) ->b-a);
+//         int res = 0;
+//         for (int i = 1; i < 26; i++) {
+//             while (freq[i] != 0 && freq[i] >= freq[i-1]) {
+//                 freq[i]--;
+//                 res++;
+//             }
+//         }
+//         return res;
+//     }
+// }
+
