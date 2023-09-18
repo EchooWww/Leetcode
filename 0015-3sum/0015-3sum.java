a3ebf226dbff5e38a3c836cc0a2177662379a1d0
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-09-17 23:44:43 -0700

    Time: 244 ms (31.18%), Space: 50.6 MB (70.14%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,25 +4,19 @@ class Solution {
         Set<List<Integer>> triples = new HashSet<>();
         for (int i = 0; i < nums.length - 2; i++) {
             if (nums[i] > 0) break;
-            List<List<Integer>> pairs = find2Pair(nums, i+1, 0 - nums[i]);
-            if (pairs.size() == 0) continue;
-            for (int j = 0; j < pairs.size(); j++) {
-                triples.add(new ArrayList(Arrays.asList(nums[i], pairs.get(j).get(0), pairs.get(j).get(1))));
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    triples.add(new ArrayList(Arrays.asList(nums[i], nums[l++], nums[r--])));
+                }
+                else if (sum < 0) l++;
+                else r--;
             }
         }
-        List<List<Integer>> res = new ArrayList<>(triples);
-        return res;
+
+        return new ArrayList(triples);
     }
 
-    public List<List<Integer>> find2Pair(int[] nums, int start, int target) {
-        int l = start, r = nums.length - 1;
-        List<List<Integer>> res =  new ArrayList<>();
-        while (l < r) {
-            int sum = nums[l] + nums[r];
-            if (sum == target) res.add(new ArrayList(Arrays.asList(nums[l], nums[r])));
-            if (sum < target) l++;
-            else r--;
-        }
-        return res;
-    }
 }
