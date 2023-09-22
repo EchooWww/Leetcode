abdbc4e24d8a4e17208fea9c485a30349c96113f
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-09-22 15:09:25 -0700

    Time: 1 ms (89.7%), Space: 40 MB (95.1%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int sp = 0, tp = 0;
-        char[] schar = s.toCharArray(), tchar = t.toCharArray();
-        while (sp < s.length()) {
-            while (tp < t.length() && schar[sp] != tchar[tp]) tp++;
-            sp++;
-            if (tp >= t.length()) return false;
-            tp++;
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) i++;
+            j++;
         }
+        if (i != s.length()) return false;
         return true;
     }
 }
