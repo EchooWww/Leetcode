e4a9a549ecb66ce323ac881582da884b5d843417
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-10-04 20:07:11 -0700

    Time: 5 ms (52.84%), Space: 47.1 MB (16.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,29 +1,19 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 0, count1 = 0, count2 = 0;
-        for(int n: nums) {
-            if (n == candidate1) count1++;
-            else if (n == candidate2) count2++;
-            else if (count1 == 0) {
-                candidate1 = n;
-                count1 ++;
-            } else if (count2 == 0) {
-                candidate2 = n;
-                count2 ++;
-            } else {
-                count1 --;
-                count2 --;
-            }
-        }
-        count1 = count2 = 0;
-        for (int n : nums) {
-            if (n == candidate1) count1++;
-            else if (n == candidate2) count2++;
-        }
-
+        Arrays.sort(nums);
+        int n = nums.length;
         List<Integer> res = new ArrayList<>();
-        if (count1 > nums.length/3) res.add(candidate1);
-        if (count2 > nums.length/3) res.add(candidate2);
+
+        int freq = 0;
+        int curr = nums[0];
+        for(int num: nums) {
+            if (num == curr) freq ++;
+            else {
+                curr = num;
+                freq = 1;
+            }
+            if (freq > n/3 && !res.contains(num)) res.add(num);
+        }
         return res;
     }
 }
