a07108860492ee9105b07dc051d59010b0fed713
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-10-06 09:17:30 -0700

    Time: 0 ms (100%), Space: 39.1 MB (51.27%) - LeetHub

--- 0343-integer-break/0343-integer-break.java
+++ 0343-integer-break/0343-integer-break.java
@@ -2,16 +2,16 @@ class Solution {
     int[] memo;
     public int integerBreak(int n) {
         memo = new int[n + 1];
-        return getHighestProduct(n);
+        return dp(n);
     }
-
-    int getHighestProduct(int n) {
+    int dp(int n) {
         if (n <= 1) return n;
         if (memo[n] > 0) return memo[n];
         int res = 0;
-        for (int i = 1; i < n + 1; i++) 
-            res = Math.max(res, i * Math.max(getHighestProduct(n-i), n-i));
+        for (int i = 1; i < n+1; i++) 
+            res = Math.max(res, i * Math.max(dp(n-i), n-i));
         memo[n] = res;
         return res;
     }
+
 }
