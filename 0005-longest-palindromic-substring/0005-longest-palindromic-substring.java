fd6d13da38d54f7ea61e35ae37ca8a852e8e3713
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2023-10-26 22:29:30 -0700

    Time: 20 ms (54.37%), Space: 43.5 MB (53.78%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,21 +1,21 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String longest = "";
-        for(int i = 0; i < s.length(); i++) {
-            String s1 = findPalindrome(s, i, i);
-            String s2 = findPalindrome(s, i, i+1);
-            longest = s1.length()>longest.length()? s1:longest;
-            longest = s2.length()>longest.length()? s2:longest;
+        String Longest = "";
+        if (s.length() <= 1) return s;
+        for (int i = 0; i < s.length()-1; i++) {
+            String odd = findPalindrome(s, i, i);
+            String even = findPalindrome(s, i, i+1);
+            Longest = odd.length() > Longest.length()? odd: Longest;
+            Longest = even.length() > Longest.length()? even: Longest;
         }
-        return longest;
-        
+        return Longest;
     }
-    
-    public String findPalindrome(String s, int l, int r) {
-        while(l >= 0 && r < s.length()&& s.charAt(l)==s.charAt(r) ) {
+
+    public String findPalindrome (String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
             l--;
             r++;
         }
         return s.substring(l+1, r);
     }
-}
+ }
