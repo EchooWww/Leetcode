bdb495a25f089f0804d2667f3680f0337a0f6cd7
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2024-01-31 20:43:59 -0800

    Time: 25 ms (38.13%), Space: 62.1 MB (5.21%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -3,14 +3,9 @@ class Solution {
         int n = nums.length;
         Arrays.sort(nums);
         int[][] res = new int[n/3][3];
-        for (int i = 0; i < n; i++) {
-            if (i % 3 == 1) {
-                if (nums[i] - nums[i-1] > k) return new int[][]{};
-            }
-            if (i % 3 == 2) {
-                if (nums[i] - nums[i-2] > k) return new int[][]{};
-            }
-            res[i/3][i%3] = nums[i];
+        for (int i = 0; i < n; i+=3) {
+            if (nums[i+2] - nums[i] <= k) res[i/3] = new int[]{nums[i], nums[i+1], nums[i+2]};
+            else return new int[0][0];
         }
         return res;
     }
