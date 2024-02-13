a010dfd5079f5a77f5fad735d47e2122cb89f098
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2024-02-12 19:31:56 -0800

    Time: 2 ms (71.53%), Space: 44.9 MB (65.4%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String s: words) {
+            if (isPalindrome(s)) return s;
+        }
+        return "";
+    }
+
+    boolean isPalindrome(String s){
+        int l = 0, r = s.length()-1;
+        while(l<=r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
