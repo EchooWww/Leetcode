e5079d69666fd6b076164978cdf9fe308d268974
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2024-02-14 23:34:18 -0800

    Time: 3 ms (100%), Space: 76.7 MB (74.08%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,31 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        int len = nums.length;
-        int[] res = new int[len];
-        int p = 0, n = 0, curr = 0;
-        for (int i = 0; i < len; i++) {
-            if (nums[i] > 0) {
-                p = i;
-                break;
+        int n = nums.length;
+        int[] res = new int[n];
+        int pos = 0, neg = 1, curr = 0;
+        while (curr < n) {
+            if (nums[curr] > 0) {
+                res[pos] = nums[curr++];
+                pos += 2;
+            } else {
+                res[neg] = nums[curr++];
+                neg += 2;
             }
         }
-        for (int i = 0; i < len; i++) {
-            if (nums[i] < 0) {
-                n = i;
-                break;
-            }
-        }
-        while (curr < len) {
-            if (p < len && curr < len) {
-                res[curr++] = nums[p++];
-                while (p < len && nums[p] < 0) p++;
-            }
-            if (n < len && curr < len) {
-                res[curr++] = nums[n++];
-                while (n < len && nums[n] > 0) n++;
-            }
 
-        }
         return res;
     }
 }
