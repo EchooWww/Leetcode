9c3be9aa7a89fbb0c06a7e5f48a3bb2e114c9813
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2024-02-19 22:23:17 -0800

    Time: 0 ms (100%), Space: 45.2 MB (48.02%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        res ^= n;
+        return res;
+    }
+}
