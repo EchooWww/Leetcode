3311bbf69d2be4312d11fe94b2034e2fc8649280
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2024-04-10 20:26:46 -0700

    Time: 20 ms (62.89%), Space: 44.6 MB (89.34%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.java
+++ 0402-remove-k-digits/0402-remove-k-digits.java
@@ -1,18 +1,30 @@
+import java.util.Stack;
+
 class Solution {
     public String removeKdigits(String num, int k) {
-        int resLen = num.length() - k;
+        int resLen = num.length() - k;        
         Stack<Character> digits = new Stack<>();
-        StringBuilder res = new StringBuilder();
+        int validDigitsStartIndex = 0;
+        
         for (char digit: num.toCharArray()) {
-            while (k > 0 && !digits.isEmpty() && digit - '0' < digits.peek() - '0') {
+            while (k > 0 && !digits.isEmpty() && digit < digits.peek()) {
                 digits.pop();
                 k--;
             }
             digits.push(digit);
         }
-        while (!digits.isEmpty()) res.insert(0, digits.pop());
-        int i = 0;
-        while (i < resLen && res.charAt(i) == '0') i++;
-        return i == resLen? "0" : res.toString().substring(i, resLen);
+        
+        char[] resArr = new char[digits.size()];
+        int index = digits.size() - 1;
+        while (!digits.isEmpty()) {
+            resArr[index--] = digits.pop();
+        }
+        
+        while (validDigitsStartIndex < resArr.length && resArr[validDigitsStartIndex] == '0') {
+            validDigitsStartIndex++;
+        }
+        if (validDigitsStartIndex >= resLen) return "0";
+        
+        return new String(resArr, 0, resLen).substring(validDigitsStartIndex);
     }
 }
