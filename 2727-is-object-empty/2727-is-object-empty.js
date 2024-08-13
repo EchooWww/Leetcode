7b935cdb1fbf8e852e83c9314e07964b21daabed
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2024-08-13 00:19:04 -0700

    Time: 57 ms (27.4%), Space: 49.2 MB (61.68%) - LeetHub

--- 2727-is-object-empty/2727-is-object-empty.js
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -4,5 +4,7 @@
  */
 var isEmpty = function(obj) {
     // return Object.keys(obj).length === 0;
-    return JSON.stringify(obj) === "{}" || JSON.stringify(obj) === "[]" ;
+    // return JSON.stringify(obj) === "{}" || JSON.stringify(obj) === "[]" ;
+    for (const _ in obj) return false;
+    return true;
 };
