b17d080a29c3ab7287aa4c113877bf8bb47a6f82
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2024-09-04 14:10:18 -0700

    Time: 170 ms (31.66%), Space: 37.1 MB (18.59%) - LeetHub

--- 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.kt
+++ 1945-sum-of-digits-of-string-after-convert/1945-sum-of-digits-of-string-after-convert.kt
@@ -1,19 +1,8 @@
 class Solution {
     fun getLucky(s: String, k: Int): Int {
-        var num:String = ""
+        var num = s.map{it - 'a' + 1}.joinToString("")
         var count = k
-        for (char in s) num += (char - 'a' + 1).toString()
-        while (count-- > 0) num = transform(num)
+        while (count-- > 0 && num.length > 1) num = num.sumOf{it.digitToInt()}.toString()
         return num.toInt()
-    }
-
-    fun transform(num:String):String {
-        var res:Int = 0
-        var number = num
-        while (number.length > 0) {
-            res += number.last().digitToInt()
-            number = number.dropLast(1)
-        }
-        return res.toString()
     }
 }
