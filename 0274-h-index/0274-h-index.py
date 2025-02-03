f40b23675503bab4ff5725d422013b0aa2c3beb7
Echo Wang <122243253+EchooWww@users.noreply.github.com>
2025-02-02 21:15:00 -0800

    Time: 0 ms (100%), Space: 18.1 MB (51.58%) - LeetHub

--- 0274-h-index/0274-h-index.py
+++ 0274-h-index/0274-h-index.py
@@ -1,11 +1,13 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        h = 0
-        # max h that: >= h papers has been cited >= h times
-        citations.sort()
-        for i in range(len(citations)):
-            h = max(h, min(len(citations) - i, citations[i]))
-
-        return h
-
-
+        # greedy solution: the maximum possible n is len(citations), so we keep another list of count frequencies
+        n = len(citations)
+        count = [0] * (n + 1)
+        currCount = 0
+        for c in citations:
+          count[min(n,c)] += 1 # if citation > n, then it should be added to n
+        for i in range(n, -1, -1):
+            currCount += count[i]
+            if currCount >= i:
+                return i
+        return currCount
